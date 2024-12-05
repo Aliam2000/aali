5e13b83ddb173352460afed30f9be4b7266146a6
ali <alishirinzadeh845@gmail.com>
2024-12-05 14:52:08 +0330

    part#7

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM, { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const tick = ()=>{
+//   const elem =(
+//     <div>
+//       <h1>hi bodies</h1>
+//       <h2>
+//         it is{new Date().toLocaleTimeString()}
+//       </h2>
+//     </div>
+
+//   )
+//   createRoot(document.getElementById("root")).render(<h1>Hi friend</h1>)
+// }
+
+// setInterval(()=>{
+//   tick();
+// },1000
+// )
+
+
